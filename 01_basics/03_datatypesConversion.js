0ff2ab31074df34a1a7cc0009d49aef3b5c1b544
PRIYAM AGARWAL <95881431+PriyamAg001@users.noreply.github.com>
2024-07-28 14:19:49 +0000

    done

--- 01_basics/03_datatypesConversion.js
+++ 01_basics/03_datatypesConversion.js
@@ -75,21 +75,23 @@ let negValue = -value
 // console.log(2/3);
 // console.log(2%3);
 
-let str1 = "hello"
-let str2 = " hitesh"
+let str1 = "Priyam"
+let str2 = " Agarwal"
 
 let str3 = str1 + str2
-// console.log(str3);
+ console.log(str3);
 
-// console.log("1" + 2);
-// console.log(1 + "2");
-// console.log("1" + 2 + 2);
-// console.log(1 + 2 + "2");
+console.log("1" + 2);           // 12
+console.log(typeof("1" + 2))    // string
+console.log(1 + "2");           // 12 
+console.log(typeof(1 + "2"))    // string
+console.log("1" + 2 + 2);       // 122
+console.log(1 + 2 + "2");       // 32 (by defined format)
 
-// console.log( (3 + 4) * 5 % 3);
+console.log( (3 + 4) * 5 % 3);
 
-// console.log(+true);
-// console.log(+"");
+console.log(+true);
+console.log(+"");
 
 let num1, num2, num3
 
