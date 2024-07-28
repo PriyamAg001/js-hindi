7b90d59c5cdcd28f55b55ceb1f1eceaa32bc23bf
PRIYAM AGARWAL <95881431+PriyamAg001@users.noreply.github.com>
2024-07-28 14:22:35 +0000

    done

--- 01_basics/03_datatypesConversion.js
+++ 01_basics/03_datatypesConversion.js
@@ -91,6 +91,7 @@ console.log(1 + 2 + "2");       // 32 (by defined format)
 console.log( (3 + 4) * 5 % 3);
 
 console.log(+true);
+// console.log(true+); // Not Possible
 console.log(+"");
 
 let num1, num2, num3
@@ -98,5 +99,7 @@ let num1, num2, num3
 num1 = num2 = num3 = 2 + 2
 
 let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
 ++gameCounter;
 console.log(gameCounter);
