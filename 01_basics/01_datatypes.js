1455a7f426a083ef5ca94abc81a607662e1a5bf8
PRIYAM AGARWAL <95881431+PriyamAg001@users.noreply.github.com>
2024-07-28 13:51:14 +0000

    config

---
+++ 01_basics/01_datatypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3 + 3) // we are uisng nodejs, not browser
+
+console.log(3 + 3); 
+console.log("Priyam");
+
+
+let name = "Priyam Agarwal"
+let age = 20;
+let isLoggedIn = false
+
+// number => 2 ^ 53
+// bigInt
+// string => ""
+// boolean => true or false
+// null => stand alone value
+// undefined => value not defined
+// symbol => uniqueness 
+
+
+// object 
+
+console.log(typeof ("Priyam"));
+console.log(typeof (18));
+console.log(typeof null)       // object
+console.log(typeof undefined)  // undefined
